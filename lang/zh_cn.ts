83504b59874c215cdc4d7b7603acb2e37e8874a2
zj <usenamespace@163.com>
2023-11-15 16:23:23 +0800

    增加 多语言配置

---
+++ lang/zh_cn.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="17"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="254"/>
+        <source>qt_uiautomatorviewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="155"/>
+        <location filename="../ui/main/mainwindow.ui" line="178"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="276"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="283"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="136"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="272"/>
+        <source>Enter keywords</source>
+        <translation type="unfinished">输入关键字</translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="162"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="278"/>
+        <source>Alt+Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="116"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="269"/>
+        <source>Expand All</source>
+        <translation type="unfinished">全部展开</translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="152"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="274"/>
+        <source>Last</source>
+        <translation type="unfinished">上一个</translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="175"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="281"/>
+        <source>Next</source>
+        <translation type="unfinished">下一个</translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="185"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="285"/>
+        <source>Alt+Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="258"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="288"/>
+        <source>Node details</source>
+        <translation type="unfinished">节点详情</translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="340"/>
+        <location filename="../ui/main/mainwindow.ui" line="343"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="255"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="257"/>
+        <source>Open</source>
+        <translation type="unfinished">打开</translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="355"/>
+        <location filename="../ui/main/mainwindow.ui" line="358"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="259"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="261"/>
+        <source>Setting</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <source>setting</source>
+        <translation type="obsolete">设置</translation>
+    </message>
+    <message>
+        <location filename="../ui/main/mainwindow.ui" line="370"/>
+        <location filename="../ui/main/mainwindow.ui" line="373"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="263"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_mainwindow.h" line="265"/>
+        <source>Screenshot</source>
+        <translation type="unfinished">截图</translation>
+    </message>
+    <message>
+        <source>screenshot</source>
+        <translation type="obsolete">截图</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="../ui/option/settingdialog.ui" line="23"/>
+        <location filename="../ui/option/settingdialog.ui" line="107"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_settingdialog.h" line="127"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_settingdialog.h" line="133"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/option/settingdialog.ui" line="40"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_settingdialog.h" line="128"/>
+        <source>Devices</source>
+        <translation type="unfinished">设备列表</translation>
+    </message>
+    <message>
+        <location filename="../ui/option/settingdialog.ui" line="47"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_settingdialog.h" line="129"/>
+        <source>Save Path</source>
+        <translation type="unfinished">保存路径</translation>
+    </message>
+    <message>
+        <location filename="../ui/option/settingdialog.ui" line="82"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_settingdialog.h" line="131"/>
+        <source>Cancel</source>
+        <translation type="unfinished">取消</translation>
+    </message>
+    <message>
+        <location filename="../ui/option/settingdialog.ui" line="95"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_settingdialog.h" line="132"/>
+        <source>Save</source>
+        <translation type="unfinished">保存</translation>
+    </message>
+    <message>
+        <location filename="../ui/option/settingdialog.ui" line="14"/>
+        <location filename="../cmake-build-debug/uav_autogen/include/ui_settingdialog.h" line="126"/>
+        <location filename="../ui/option/settingdialog.cpp" line="10"/>
+        <source>Setting</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+</context>
+</TS>
